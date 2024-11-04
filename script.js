da90876ab427a6d4a4a9f5979848d3f99e819a64
Toktobekova-Nora-dev <nuraiymtoktobekkyzy507@gmail.com>
2024-11-04 09:47:23 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,69 @@
+let res = document.querySelectorAll(".container");
+const clock = document.querySelector(".clock");
+const first = document.querySelector(".first")
+const second = document.querySelector(".second");
+const three = document.querySelector(".three");
+const four = document.querySelector(".four");
+const five = document.querySelector(".five");
+const six = document.querySelector(".six");
+const seven = document.querySelector(".seven");
+const eithg = document.querySelector(".eithg")
+const nine = document.querySelector(".nine");
+const ten = document.querySelector(".ten");
+first.addEventListener("click",()=>{
+    clock.style.marginBottom = "0%"
+    clock.style.transition = "2s"
+})
+
+second.addEventListener("click",()=>{
+clock.style.marginBottom = "70px"
+clock.style.transition = "2s"
+})
+
+three.addEventListener("click",()=>{
+    clock.style.marginBottom = "140px"
+    clock.style.transition = "2s"
+})
+
+
+four.addEventListener("click",()=>{
+    clock.style.marginBottom = "210px"
+    clock.style.transition = "2s"
+})
+
+five.addEventListener("click",()=>{
+    clock.style.marginBottom = "280px"
+    clock.style.transition = "2s"
+})
+   
+
+six.addEventListener("click",()=>{
+    clock.style.marginBottom = "350px"
+    clock.style.transition = "2s"
+})
+
+
+seven.addEventListener("click",()=>{
+    clock.style.marginBottom = "420px"
+    clock.style.transition = "2s"
+})
+
+
+eithg.addEventListener("click",()=>{
+    clock.style.marginBottom = "490px"
+    clock.style.transition = "2s"
+})
+  
+
+
+
+nine.addEventListener("click",()=>{
+    clock.style.marginBottom = "560px"
+    clock.style.transition = "2s"
+})
+   
+ten.addEventListener("click",()=>{
+    clock.style.marginBottom = "630px"
+    clock.style.transition = "2s"
+})
+
